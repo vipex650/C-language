0b6ba801cffeb4e3714e64e12df4a4170bd2d8bd
ANUJ GUPTA <anujmodanwal708042@gmail.com>
2025-02-18 19:07:25 +0530

    sum of digit

--- Basic/SumOfdigit.c
+++ Basic/SumOfdigit.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+
+void SumOfDigit(int n){
+    int sum=0,a=0;
+    while(n!=0)
+        {
+            a=n%10;
+            sum=sum+a;
+            n=n/10;  
+        }
+    printf("%d",sum); 
+}
+
+
+
 int main()
 {
-    int n=54321,sum=0,a=0;
-    while(n!=0)
-    {
-        a=n%10;
-        sum=sum+a;
-        n=n/10;
-        
-    }
-    printf("%d",sum);
+    int n=54321;
+    SumOfDigit(n);
     return 0;
 }
